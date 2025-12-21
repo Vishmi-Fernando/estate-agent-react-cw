7fcffe3d380f7c545369037065004b6ca70f7847
Vishmi Fernando <vishmiwarsha94@gmail.com>
2025-12-21 20:22:26 +0530

    Set up App foundation and basic SearchForm

---
+++ src/components/search/SearchForm.jsx
@@ -0,0 +1,10 @@
+function SearchForm() {
+  return (
+    <form className="search-form">
+      <h2>Search Properties</h2>
+      <p>Search form coming next...</p>
+    </form>
+  );
+}
+
+export default SearchForm;
